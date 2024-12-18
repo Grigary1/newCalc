1fa8304c64b076a89fb8e76ecc84c6b9ccacb1d7
Grigary1 <grigaryvarkey16@gmail.com>
2024-12-18 11:59:22 +0530

    feature added

---
+++ app/src/main/java/com/hospital/newcalculator/AddActivity.java
@@ -0,0 +1,45 @@
+package com.hospital.newcalculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddActivity extends AppCompatActivity {
+    Button b1;
+    TextView t1;
+    EditText e1,e2;
+    String getNum1,getNum2,result;
+    int num1,num2,sum;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add);
+        e1=(EditText) findViewById(R.id.addN1);
+        e2=(EditText) findViewById(R.id.addN2);
+        t1=(TextView) findViewById(R.id.showAddRes);
+        b1=(Button) findViewById(R.id.addRes);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getNum1=e1.getText().toString();
+                getNum2=e2.getText().toString();
+                num1=Integer.parseInt(getNum1);
+                num2=Integer.parseInt(getNum2);
+                sum=num1+num2;
+                result=String.valueOf(sum);
+                Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+                t1.setText(result);
+            }
+        });
+    }
+}
--- app/src/main/java/com/hospital/newcalculator/MainActivity.java
+++ app/src/main/java/com/hospital/newcalculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.hospital.newcalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1,b2,b3,b4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        b1=(Button) findViewById(R.id.aadd);
+        b2=(Button) findViewById(R.id.sub);
+        b3=(Button) findViewById(R.id.mul);
+        b4=(Button) findViewById(R.id.div);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob=new Intent(getApplicationContext(), AddActivity.class);
+                startActivity(ob);
+            }
+        });
     }
 }
